d768bd75f134853767d18ea57bdfe728e6ba4dd0
Aman <aman.sharma@eazydiner.com>
2025-05-30 11:59:05 +0530

    refactor

--- scripts/Ingestion/fetch_data.py
+++ scripts/Ingestion/fetch_data.py
@@ -10,6 +10,7 @@ resources_files = {
     "Year_wise_Details_of_Funds_Allocated_by_the_Archaeological_Survey_of_India_(ASI)_for_Cultural_Heritage_Centers_and_other_Places_of_Heritage_Importance_Identified_from_2019-20_to_2022-23": "/resource/03d57ebd-7271-4a37-8204-6309719b8d67", 
     "Inbound Tourism Foreign Tourist Arrivals, Arrivals of Non-Resident Indians and International Tourist Arrivals 1981-2020": "/resource/5015c014-507e-4ddf-8051-2c92dd426d38",
     "Month wise break up of Non Residents Indians arrivals 2018-2020": "/resource/8db754ef-1776-416a-bfbd-bc5696e31492", 
+    "State-UT-wise Number of Beneficiaries and Funds Released for Preservation and Development of Cultural Heritage of the Himalayas from 2019-20 to 2023-24": "/resource/3ab1022f-9a10-4b26-b367-b8a8376df797",
 }
 
 for dataset_name, resource in resources_files.items():
@@ -37,9 +38,10 @@ for dataset_name, resource in resources_files.items():
             row_exists = False
 
     time.sleep(1)    
-    os.makedirs(f"data/{dataset_name}/raw", exist_ok=True)
+    output_dir = f"data/bronze/{dataset_name}/"
+    os.makedirs(output_dir, exist_ok=True)
 
-    with open(f"data/{dataset_name}/raw/{dataset_name}.json", "w") as dataset_records:
+    with open(f"{output_dir}/{dataset_name}.json", "w") as dataset_records:
         json.dump(dataset, dataset_records, indent=4)
     print(f"Dumped {dataset_name} data: {response.status_code}\n")
 
